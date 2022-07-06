7ea243ece7210fb22d564e62ddd7bdb156d9122f
Leandro Patrício <69220785+Leandro-Patricio@users.noreply.github.com>
2022-07-06 16:52:34 -0300

    Delete exercicio4entregue directory

--- exercicio4entregue/src/apps/api/routes/ArrayRoute.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { PostArrayController } from "../controllers/PostArrayController";
-
-const arrayRouter = express.Router();
-const _controllerPost = new PostArrayController();
-
-arrayRouter.post("/reordena", _controllerPost.getReordenaAll);
-
-export = arrayRouter;
--- exercicio4entregue/src/apps/api/routes/index.ts
+++
@@ -1,4 +0,0 @@
-import arrayRouter from "./ArrayRoute";
-import lotemIpsumRouter from "./LoremIpsumRoute";
-
-export default [arrayRouter, lotemIpsumRouter];
